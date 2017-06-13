d0ef4a369fa4c1feff0cb1faba4aefb0a75d968f
袁其亮 <yuanqiliang@51kik.com>
2017-06-13 16:04:01 +0800

    executor demo

---
+++ src/main/java/com/lance/demo/java/App.java
@@ -0,0 +1,18 @@
+package com.lance.demo.java;
+
+import java.util.concurrent.*;
+
+/**
+ * Created by perdonare on 2017/5/27.
+ */
+public class App {
+    public static void main(String[] args) {
+        ExecutorService executorService;
+        ScheduledExecutorService scheduledExecutorService;
+        Executor executor;
+        RunnableFuture<String> runnableFuture;
+        ThreadPoolExecutor threadPoolExecutor;
+        Future future;
+        Callable<String> callable;
+    }
+}
---
+++ src/main/java/com/lance/demo/java/executor/CallableDemo.java
@@ -0,0 +1,11 @@
+package com.lance.demo.java.executor;
+
+/**
+ * Created by perdonare on 2017/5/27.
+ */
+public class CallableDemo {
+    public String call() throws Exception {
+        
+        return null;
+    }
+}
