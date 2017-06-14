858d98d3993a36024df8416cfed82e99e217c717
袁其亮 <yuanqiliang@51kik.com>
2017-06-14 11:07:47 +0800

    init maven project

---
+++ src/main/java/com/lance/demo/java/App.java
@@ -0,0 +1,10 @@
+package com.lance.demo.java;
+
+/**
+ * Created by perdonare on 2017/6/14.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
