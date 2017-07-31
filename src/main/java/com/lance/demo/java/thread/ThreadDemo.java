65eb891fd9ab0f71781e998b62b21b21ee81ef0b
袁其亮 <yuanqiliang@51kik.com>
2017-07-31 16:38:44 +0800

    wait notify

--- src/main/java/com/lance/demo/java/thread/ThreadDemo.java
+++ src/main/java/com/lance/demo/java/thread/ThreadDemo.java
@@ -1,10 +1,17 @@
 package com.lance.demo.java.thread;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by perdonare on 2017/6/21.
  */
 public class ThreadDemo {
-
+    private final Object object = new Object();
+    private int count;
     class ThreadInstance extends Thread{
         @Override
         public void run() {
@@ -79,5 +86,35 @@ public class ThreadDemo {
         ThreadInstance3 threadInstance = new ThreadInstance3();
         threadInstance.start();
         return threadInstance;
+    }
+
+    public void notifyDemo() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 5; i++) {
+            executorService.execute(()->{
+                try {
+                    supportNotify();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+
+    }
+
+    private void supportNotify() throws InterruptedException {
+        synchronized (object) {
+            System.out.println("thread enter: "+ Thread.currentThread().getName());
+            count++;
+            if (count == 5) {
+                // object.notify();
+                object.notifyAll();
+                System.out.println("thread notify: "+ Thread.currentThread().getName());
+            }
+            System.out.println("thread wait: "+ Thread.currentThread().getName());
+            object.wait();
+        }
+        System.out.println("thread out: "+ Thread.currentThread().getName());
     }
 }
--- src/test/java/com/lance/demo/java/thread/ThreadDemoTest.java
+++ src/test/java/com/lance/demo/java/thread/ThreadDemoTest.java
@@ -8,9 +8,9 @@ import static org.junit.Assert.*;
  * Created by perdonare on 2017/6/21.
  */
 public class ThreadDemoTest {
+    ThreadDemo threadDemo = new ThreadDemo();
     @Test
     public void interrupted() throws Exception {
-        ThreadDemo threadDemo = new ThreadDemo();
         ThreadDemo.ThreadInstance interrupted = threadDemo.interrupted();
         Thread.sleep(1000l);
         interrupted.interrupt();
@@ -19,20 +19,24 @@ public class ThreadDemoTest {
 
     @Test
     public void interrupted2() throws Exception {
-        ThreadDemo threadDemo = new ThreadDemo();
         ThreadDemo.ThreadInstance2 interrupted = threadDemo.interrupted2();
-        Thread.sleep(1000l);
+        Thread.sleep(1000);
         interrupted.interrupt();
-        Thread.sleep(1000l);
+        Thread.sleep(1000);
     }
 
 
     @Test
     public void interrupted3() throws Exception {
-        ThreadDemo threadDemo = new ThreadDemo();
         ThreadDemo.ThreadInstance3 interrupted = threadDemo.interrupted3();
-        Thread.sleep(1000l);
+        Thread.sleep(1000);
         interrupted.interrupt();
-        Thread.sleep(1000l);
+        Thread.sleep(1000);
+    }
+
+    @Test
+    public void notifyDemo() throws Exception {
+        threadDemo.notifyDemo();
+        Thread.sleep(50000);
     }
 }
